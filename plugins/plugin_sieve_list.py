6b03f77aa8b5a149a25799e869ffb81434320817
pershyna <pershyna@node0.pershyna-298287.cs2640-pg0.cloudlab.umass.edu>
2026-04-05 07:06:25 -0600

    filled in random file and re-did sieve algorithm using list queue

--- plugins/plugin_sieve_list.py
+++ plugins/plugin_sieve_list.py
@@ -1,32 +1,44 @@
 from collections import deque
 from libcachesim import CommonCacheParams, Request
 import random
+import math
 
 class FifoCache:
     def __init__(self, cache_size: int):
-        self.queue = deque()
+        self.queue = []
         self.cache_size = cache_size
+        self.hand = 0
+        self.tracker_array = []
 
     def on_hit(self, req: Request):
-        pass  # FIFO doesn't reorder on hit
+        index_visited = self.queue.index(req.obj_id)
+        self.tracker_array[index_visited] = 1
 
     def on_miss(self, req: Request):
         if req.obj_size <= self.cache_size:
             self.queue.append(req.obj_id)
+            self.tracker_array.append(0)
 
     def evict(self, req: Request):
         if not self.queue:
             return 0
-        idx = random.randrange(len(self.queue))
-        self.queue.rotate(-idx)
-        victim = self.queue.popleft()
-        self.queue.rotate(idx)
-
+        hand = self.hand
+        if hand > len(self.queue) - 1:
+            hand = 0
+        length = len(self.queue)
+        while self.tracker_array[hand] == 1:
+            self.tracker_array[hand] = 0
+            hand = (hand + 1) % length
+        victim = self.queue.pop(hand)
+        self.tracker_array.pop(hand)
+        self.hand = hand
         return victim
 
     def on_remove(self, obj_id: int):
         try:
-            self.queue.remove(obj_id)
+           index_removed = self.queue.index(obj_id) 
+           self.queue.pop(index_removed)
+           self.tracker_array.pop(index_removed)
         except ValueError:
             pass  # Object not in queue
 
